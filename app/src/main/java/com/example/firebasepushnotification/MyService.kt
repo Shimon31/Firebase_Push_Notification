c02eddb43c2516fe3f731994b77b92df04bb4487
Shimon <shimonbhuiya02@gmail.com>
2025-04-29 15:16:44 +0600

    Full Complete

--- app/src/main/java/com/example/firebasepushnotification/MyService.kt
+++ app/src/main/java/com/example/firebasepushnotification/MyService.kt
@@ -20,7 +20,64 @@ class MyService : FirebaseMessagingService() {
         Log.d("TAG", "onNewToken: $token")
     }
 
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
 
+        val title = message.notification?.title
+        val body = message.notification?.body
 
+        Log.d("TAG", "title: $title ")
+        Log.d("TAG", "body: $body")
+
+        showNotification(title, body)
+
+    }
+
+    private fun showNotification(title: String?, body: String?) {
+
+        var channel: NotificationChannel? = null
+        var builder: NotificationCompat.Builder? = null
+
+        val channelId = "com.example.firebasepushnotification.test-notification"
+
+        val manager: NotificationManager =
+            getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        val intent = Intent(this@MyService, MainActivity2::class.java)
+
+        val pendingIntent = PendingIntent.getActivity(
+            this@MyService,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
+        )
+
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+
+            channel = NotificationChannel(channelId,channelId,NotificationManager.IMPORTANCE_HIGH)
+            manager.createNotificationChannel(channel)
+
+            builder = NotificationCompat.Builder(this@MyService,channelId)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setSmallIcon(R.drawable.baseline_doorbell_24)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+
+        }else{
+
+            builder = NotificationCompat.Builder(this@MyService)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setSmallIcon(R.drawable.baseline_doorbell_24)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+
+        }
+        val id  = System.currentTimeMillis().toInt()
+
+        manager.notify(id,builder.build())
+
+    }
 
 }
